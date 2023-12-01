4c8c56dc1035223cda3087116c8a6e0e68be4371
Melchi M. Michel <melchimm@gmail.com>
2023-12-01 12:38:34 -0500

    set up to successfully compile for Python 3.10 on Ubuntu 22.04

---
+++ .aksetup-defaults.py
@@ -0,0 +1,4 @@
+BOOST_BINDINGS_INC_DIR = ['/usr/include/boost/numeric-bindings']
+BOOST_INC_DIR = ['/usr/include/boost']
+BOOST_LIB_DIR = ['/usr/lib/x86_64-linux-gnu']
+BOOST_PYTHON_LIBNAME = ['boost_python310']
