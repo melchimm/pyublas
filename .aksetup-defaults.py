2dfc8895fb31c24ecf7bef5548a280bf54d58c9b
Melchi M. Michel <melchimm@gmail.com>
2024-06-08 13:54:41 -0400

    modified .aksetup-defaults.py to have more useful info

--- .aksetup-defaults.py
+++ .aksetup-defaults.py
@@ -1,4 +1,8 @@
 BOOST_BINDINGS_INC_DIR = ['/usr/include/boost/numeric-bindings']
 BOOST_INC_DIR = ['/usr/include/boost']
+# for Python 2.7
+#BOOST_LIB_DIR = ['/usr/local/lib']
+#BOOST_PYTHON_LIBNAME = ['boost_python27']
+# for Python 3.10
 BOOST_LIB_DIR = ['/usr/lib/x86_64-linux-gnu']
 BOOST_PYTHON_LIBNAME = ['boost_python310']
